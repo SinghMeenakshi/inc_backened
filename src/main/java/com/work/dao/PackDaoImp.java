7884b174bb76382d3820d4c00de6b915634b585c
MEENAKSHI <MEENAKSHI@SATNAAM-PC>
2017-07-11 16:59:53 +0530

    11/7/17

--- src/main/java/com/work/dao/PackDaoImp.java
+++ src/main/java/com/work/dao/PackDaoImp.java
@@ -1,14 +1,15 @@
 package com.work.dao;
 
-import org.hibernate.SessionFactory;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.work.model.Pack;
 
 @Repository
-
+@Transactional
 public class PackDaoImp implements PackDao
 {
 		
--- src/main/java/com/work/inc_backend/App.java
+++ src/main/java/com/work/inc_backend/App.java
@@ -2,7 +2,10 @@ package com.work.inc_backend;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.work.configuration.DBConfiguration;
+import com.work.dao.PackDaoImp;
+import com.work.service.PackServiceImp;
 
 
 public class App 
@@ -10,6 +13,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context=new AnnotationConfigApplicationContext(DBConfiguration.class);
+        ApplicationContext context=new AnnotationConfigApplicationContext(DBConfiguration.class,PackDaoImp.class,PackServiceImp.class);
     }
 }
